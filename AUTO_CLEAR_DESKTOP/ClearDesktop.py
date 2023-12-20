025a095fcf8ec68cc0975b6f3a384c8c0e9c9d7c
Ondřej Losenský <127244546+OndrejLosensky@users.noreply.github.com>
2023-12-20 22:20:49 +0100

    Update ClearDesktop.py

--- AUTO_CLEAR_DESKTOP/ClearDesktop.py
+++ AUTO_CLEAR_DESKTOP/ClearDesktop.py
@@ -7,16 +7,22 @@ def move_files(source_folder, destination_folder, extensions):
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
-    # List of all files in the source folder
-    files = os.listdir(source_folder)
+    # List of all files and folders in the source folder
+    items = os.listdir(source_folder)
 
-    # Move files to the specified folder
-    for file in files:
-        if file.endswith(extensions):
-            source_path = os.path.join(source_folder, file)
-            destination_path = os.path.join(destination_folder, file)
-            shutil.move(source_path, destination_path)
-            print(f"Moved: {file}")
+    # Move files and folders to the specified folder
+    for item in items:
+        item_path = os.path.join(source_folder, item)
+        if os.path.isfile(item_path) and item.endswith(extensions):
+            # Move files
+            destination_path = os.path.join(destination_folder, item)
+            shutil.move(item_path, destination_path)
+            print(f"Moved file: {item}")
+        elif os.path.isdir(item_path):
+            # Move folders
+            destination_path = os.path.join(destination_folder, item)
+            shutil.move(item_path, destination_path)
+            print(f"Moved folder: {item}")
 
 def monitor_and_move(desktop_path, destination_folder, extensions):
     while True:
@@ -26,6 +32,6 @@ def monitor_and_move(desktop_path, destination_folder, extensions):
 if __name__ == "__main__":
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
     destination_folder = os.path.join(desktop_path, "_old")
-    allowed_extensions = (".jpg", ".png", ".txt", ".pdf", ".webp", ".docx", ".xlsx", ".ai", ".zip", ".rtf")
+    allowed_extensions = (".jpg", ".png", ".txt", ".pdf", ".webp", ".docx", ".xlsx", ".ai", ".zip", ".rtf", ".mp4")
 
     monitor_and_move(desktop_path, destination_folder, allowed_extensions)
